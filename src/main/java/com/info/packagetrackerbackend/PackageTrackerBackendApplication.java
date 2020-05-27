8e83c027db19fd3fd9089783c20424eb04b5f276
PawelRadawiec <pawel.radaw@gmail.com>
2020-05-27 19:03:04 +0200

    first commit

---
+++ src/main/java/com/info/packagetrackerbackend/PackageTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.info.packagetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/packagetrackerbackend/PackageTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.packagetrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
