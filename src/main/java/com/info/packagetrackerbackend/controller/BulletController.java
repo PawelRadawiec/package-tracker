2c08be80a513e5029285fe5bc5b05484a014079b
PawelRadawiec <pawel.radaw@gmail.com>
2020-07-08 19:56:22 +0200

    bullet / service, controller, mocked bullets list

---
+++ src/main/java/com/info/packagetrackerbackend/controller/BulletController.java
@@ -0,0 +1,28 @@
+package com.info.packagetrackerbackend.controller;
+
+import com.info.packagetrackerbackend.model.Bullet;
+import com.info.packagetrackerbackend.service.BulletService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/bullet/")
+public class BulletController {
+
+    private BulletService service;
+
+    public BulletController(BulletService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "list")
+    public ResponseEntity<List<Bullet>> getBullets() {
+        return new ResponseEntity<>(service.getBullets(), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/info/packagetrackerbackend/service/BulletService.java
@@ -0,0 +1,57 @@
+package com.info.packagetrackerbackend.service;
+
+import com.info.packagetrackerbackend.model.Bullet;
+import com.info.packagetrackerbackend.model.BulletType;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class BulletService {
+
+    public List<Bullet> getBullets() {
+        return Arrays.asList(
+                new Bullet(BulletType.START, getHeader(BulletType.START), getContent(BulletType.START)),
+                new Bullet(BulletType.WAREHOUSE,  getHeader(BulletType.WAREHOUSE), getContent(BulletType.WAREHOUSE)),
+                new Bullet(BulletType.SORTING_PLANT, getHeader(BulletType.SORTING_PLANT), getContent(BulletType.SORTING_PLANT)),
+                new Bullet(BulletType.TRANSPORT,  getHeader(BulletType.TRANSPORT), getContent(BulletType.TRANSPORT)),
+                new Bullet(BulletType.PARCEL_LOCKER,  getHeader(BulletType.PARCEL_LOCKER), getContent(BulletType.PARCEL_LOCKER))
+        );
+    }
+
+    private String getHeader(BulletType type) {
+        switch (type) {
+            case START:
+                return "Process start";
+            case WAREHOUSE:
+                return "Warehouse";
+            case SORTING_PLANT:
+                return "Sorting plant";
+            case TRANSPORT:
+                return "Transport";
+            case PARCEL_LOCKER:
+                return "Parcel locker";
+            default:
+                return null;
+        }
+    }
+
+    private String getContent(BulletType type) {
+        switch (type) {
+            case START:
+                return "We will take care of your package";
+            case WAREHOUSE:
+                return "Package in warehouse";
+            case SORTING_PLANT:
+                return "Package in sorting plant";
+            case TRANSPORT:
+                return "Package in transport";
+            case PARCEL_LOCKER:
+                return "Package inside parcel locker";
+            default:
+                return null;
+        }
+    }
+
+}
