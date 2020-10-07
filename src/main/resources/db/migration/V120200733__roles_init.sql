9fed901d48b4bd4a8dfccf0af6d936f5d284d1d8
PawelRadawiec <pawel.radaw@gmail.com>
2020-10-07 07:41:12 +0200

    flywaydb / roles migration

---
+++ src/main/resources/db/migration/V120200733__roles_init.sql
@@ -0,0 +1,3 @@
+insert into public.roles (id, name) values(1, 'ROLE_USER');
+insert into public.roles (id, name) values(2, 'ROLE_MODERATOR');
+insert into public.roles (id, name) values(3, 'ROLE_ADMIN');
